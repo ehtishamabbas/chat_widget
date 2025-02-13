1ee31fc6a9ea4909cac43f802a5ba92a91425f3c
ehtishamabbas <ehtishamabbas918@gmail.com>
2025-02-13 19:05:25 +0500

    .

--- embed.js
+++ embed.js
@@ -10,25 +10,20 @@
     chatContainer.style.zIndex = "9999";
 
     // Load the chat widget HTML
-    fetch("https://ehtishamabbas.github.io/chat_widget/index.html") // Update with your hosted URL
+    fetch("https://ehtishamabbas.github.io/chat_widget/index.html") 
         .then(response => response.text())
         .then(html => {
-             // Replace relative asset paths
-             html = html.replace(/(src|href)="assets\//g, `$1="https://ehtishamabbas.github.io/chat_widget/assets/`);
+            chatContainer.innerHTML = html;
 
-            //  // Additionally handle inline CSS background images and other asset-related attributes
-            //  html = html.replace(/url\(['"]?assets\//g, `url('https://ehtishamabbas.github.io/chat_widget/assets/`);
-
-             chatContainer.innerHTML = html;
             // Append styles to the page
             var link = document.createElement("link");
             link.rel = "stylesheet";
-            link.href = "https://ehtishamabbas.github.io/chat_widget/style.css"; // Update URL
+            link.href = "https://ehtishamabbas.github.io/chat_widget/style.css";
             document.head.appendChild(link);
 
             // Append scripts to the page
             var script = document.createElement("script");
-            script.src = "https://ehtishamabbas.github.io/chat_widget/script.js"; // Update URL
+            script.src = "https://ehtishamabbas.github.io/chat_widget/script.js";
             document.body.appendChild(script);
         })
         .catch(error => console.error("Error loading chat widget:", error));
