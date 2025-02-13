c0b263a5243e79ee9c2c8b0963510942c0be47bb
ehtishamabbas <ehtishamabbas918@gmail.com>
2025-02-13 19:39:36 +0500

    .

--- embed.js
+++ embed.js
@@ -17,8 +17,8 @@
             .then(html => {
                 chatContainer.innerHTML = html;
 
-                // Fix relative asset paths (only needed if they don't load properly)
-                chatContainer.innerHTML = chatContainer.innerHTML.replace(/(src|href)="assets\//g, `$1="https://ehtishamabbas.github.io/chat_widget/assets/`);
+                // // Fix relative asset paths (only needed if they don't load properly)
+                // chatContainer.innerHTML = chatContainer.innerHTML.replace(/(src|href)="assets\//g, `$1="https://ehtishamabbas.github.io/chat_widget/assets/`);
                 // Append styles to the page
                 var link = document.createElement("link");
                 link.rel = "stylesheet";
