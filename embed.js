a87937adab38818c6a25e06bef2d0950661fbc9e
ehtishamabbas <ehtishamabbas918@gmail.com>
2025-02-13 18:20:42 +0500

    .

--- embed.js
+++ embed.js
@@ -19,20 +19,7 @@
         `$1="https://ehtishamabbas.github.io/chat_widget/assets/`
       );
 
-      // Additionally handle inline CSS background images and other asset-related attributes
-      //    html = html.replace(/url\(['"]?assets\//g, `url('https://ehtishamabbas.github.io/chat_widget/assets/`);
-
-      // Update the specific src for host-image explicitly
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-
-      const hostImage = doc.querySelector("#host-image");
-      if (hostImage) {
-        hostImage.src =
-          "https://ehtishamabbas.github.io/chat_widget/assets/images/host_default.svg";
-      }
-
-      chatContainer.innerHTML = doc.body.innerHTML;
+      chatContainer.innerHTML = html;
 
       // Append styles to the page
       var link = document.createElement("link");
